00bf9882b0ea8ec6dd910897c57f280ec495e280
Martoko <Martoko@users.noreply.github.com>
2021-06-06 10:41:38 +0200

    Non-blocking terminal input

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,23 @@
-#![allow(dead_code, unused_imports, unused_variables)]
+#![allow(dead_code, unused_imports, unused_variables, unused_mut)]
 
 use std::net::{TcpListener, TcpStream, SocketAddr};
 use std::str::FromStr;
-use std::sync::mpsc::{Sender, Receiver, channel, TryRecvError};
+use std::sync::mpsc::{Sender, Receiver, channel, TryRecvError, SendError};
 use std::sync::{mpsc, Arc, RwLock};
-use std::thread::{spawn, sleep};
+use std::thread::{spawn, sleep, JoinHandle};
 use std::io::{Write, BufRead, BufReader, BufWriter, Read, stdin, stdout, ErrorKind};
 use std::error::Error;
 use sha2::{Sha512, Digest, Sha256};
 use hex::ToHex;
 use std::env::args;
 use std::time::{Duration, SystemTime, UNIX_EPOCH};
-use std::{time, fmt};
+use std::{time, fmt, io};
 use sha2::digest::generic_array::GenericArray;
 use sha2::digest::consts::{U64};
 use std::convert::TryInto;
 use std::fmt::{Display, Formatter};
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::process::exit;
 
 fn hashing() {
     let a = Block {
@@ -23,6 +25,7 @@ fn hashing() {
         parent_hash: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         nonce: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         time: SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs(),
+        difficulty: 1,
         parent: None,
     };
     sleep(Duration::from_secs(1));
@@ -31,6 +34,7 @@ fn hashing() {
         parent_hash: a.hash(),
         nonce: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         time: SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs(),
+        difficulty: 1,
         parent: Some(&a),
     };
     eprintln!("{}", hex::encode(a.hash()));
@@ -42,6 +46,7 @@ struct Block<'a> {
     parent_hash: [u8; 32],
     miner_address: [u8; 32],
     nonce: [u8; 32],
+    difficulty: u64,
     time: u64,
 
     parent: Option<&'a Block<'a>>,
@@ -53,7 +58,8 @@ impl<'a> Block<'a> {
         sha256.update(self.parent_hash);
         sha256.update(self.miner_address);
         sha256.update(self.nonce);
-
+        sha256.update(self.difficulty.to_le_bytes());
+        sha256.update(self.time.to_le_bytes());
         sha256.finalize().into()
     }
 }
@@ -61,7 +67,7 @@ impl<'a> Block<'a> {
 impl<'a> fmt::Display for Block<'a> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "({}, {}, {}, {}, {})",
-                hex::encode(self.hash()),
+               hex::encode(self.hash()),
                hex::encode(self.parent_hash),
                hex::encode(self.miner_address),
                hex::encode(self.nonce),
@@ -75,84 +81,125 @@ enum MessageType {
 }
 
 
+fn accept_connection(stream: TcpStream) -> (JoinHandle<io::Result<()>>, Sender<MessageType>) {
+    stream.set_nonblocking(true).unwrap();
+    let peer_addr = stream.peer_addr().unwrap();
+    let local_addr = stream.local_addr().unwrap();
+    println!("connection: them {}, us {}", peer_addr, local_addr);
+    let (connection_sender, connection_receiver) = channel();
+    let connection_thread = spawn(move || -> io::Result<()> {
+        let mut reader = BufReader::new(&stream);
+        let mut writer = BufWriter::new(&stream);
+
+        writer.write_fmt(format_args!("Hello, {}!\n", peer_addr))
+            .unwrap();
+        writer.flush().unwrap();
+
+        loop {
+            // match connection_receiver.try_recv() {
+            match connection_receiver.recv() {
+                Ok(MessageType::Quit) => {
+                    eprintln!("Quitting connection thread {}->{}", local_addr, peer_addr);
+                    break;
+                }
+                Ok(MessageType::SendChat(text)) => {
+                    write!(&mut writer, "[{}] {}\n", peer_addr, text).unwrap();
+
+                    if let Err(err) = writer.flush() {
+                        eprintln!("Connection closed {}->{}: {}",
+                                  local_addr, peer_addr, err);
+                        break;
+                    }
+                }
+                // Err(e) if e == TryRecvError::Empty => {
+                //     // TODO: Replace with something smarter and OS dependant
+                //     sleep(Duration::from_millis(10));
+                // }
+                Err(e) => panic!("{}", e),
+            }
+        }
+
+        Ok(())
+    });
+
+    (connection_thread, connection_sender)
+}
+
+// TODO: Use buffer?
+pub fn read_line() -> crossterm::Result<String> {
+    let mut line = String::new();
+    while let crossterm::event::Event::Key(crossterm::event::KeyEvent { code, .. }) = crossterm::event::read()? {
+        match code {
+            crossterm::event::KeyCode::Enter => {
+                break;
+            }
+            crossterm::event::KeyCode::Char(c) => {
+                line.push(c);
+            }
+            _ => {}
+        }
+    }
+
+    Ok(line)
+}
+
 fn main() -> std::io::Result<()> {
     hashing();
 
     let args: Vec<String> = args().collect();
     let listen_addr_str = args.get(1).expect("You must supply a listen addr as arg1");
-    let listener = TcpListener::bind(
-        SocketAddr::from_str(listen_addr_str).expect("Invalid address")
-    ).unwrap();
+    let listener = TcpListener::bind(listen_addr_str).unwrap();
     listener.set_nonblocking(true).unwrap();
 
-    // let (incoming_messages_sender, incoming_messages_receiver) = channel();
+    // let (main_sender, main_receiver) = channel();
 
     let (server_sender, server_receiver) = channel();
-    let server_thread = spawn(move || {
+    let server_thread = spawn(move || -> std::io::Result<()> {
         let mut connection_threads = Vec::new();
 
+        // Make outbound connections
+        &args.get(2).map(|connect_addr_str| {
+            eprintln!("Connecting to {}", connect_addr_str);
+            let mut stream = match TcpStream::connect(connect_addr_str) {
+                Ok(stream) => connection_threads.push(accept_connection(stream)),
+                Err(error) => eprintln!("Failed to connect to {}: {}", connect_addr_str, error)
+            };
+        });
+
+        // Accept new inbound connections
         for stream in listener.incoming() {
             match stream {
                 Ok(stream) => {
-                    stream.set_nonblocking(true).unwrap();
-                    println!(
-                        "connection from {} to {}",
-                        stream.peer_addr().unwrap(),
-                        stream.local_addr().unwrap()
-                    );
-                    let (connection_sender, connection_receiver) = channel();
-                    let connection_thread = spawn(move || {
-                        let mut reader = BufReader::new(&stream);
-                        let mut writer = BufWriter::new(&stream);
-
-                        writer.write_fmt(format_args!("Hello, {}!\n", stream.peer_addr().unwrap()))
-                            .unwrap();
-                        writer.flush().unwrap();
-
-                        loop {
-                            match connection_receiver.try_recv() {
-                                Ok(MessageType::Quit) => {
-                                    println!("Quitting connection thread");
-                                    break;
-                                }
-                                Ok(MessageType::SendChat(text)) => {
-                                    writer.write_fmt(format_args!(
-                                        "[{}] {}\n",
-                                        stream.peer_addr().unwrap(),
-                                        text
-                                    )).unwrap();
-                                    writer.flush().unwrap();
-                                }
-                                Err(e) if e == TryRecvError::Empty => {
-                                    // TODO: Replace with something smarter
-                                    sleep(Duration::from_millis(10));
-                                }
-                                Err(e) => panic!("{}", e),
-                            }
-                        }
-                    });
-                    connection_threads.push((connection_thread, connection_sender));
+                    connection_threads.push(accept_connection(stream));
                 }
                 Err(ref e) if e.kind() == ErrorKind::WouldBlock => {
                     match server_receiver.try_recv() {
                         Ok(MessageType::Quit) => {
-                            for (_, connection_sender) in connection_threads.iter() {
-                                connection_sender.send(MessageType::Quit).unwrap();
-                            }
+                            connection_threads.retain(|(_, connection_sender)| {
+                                if let Err(_) = connection_sender.send(MessageType::Quit) {
+                                    false
+                                } else {
+                                    true
+                                }
+                            });
 
                             while let Some((connection_thread, _)) = connection_threads.pop() {
-                                connection_thread.join().unwrap();
+                                connection_thread.join().unwrap().unwrap();
                             }
-                            println!("Quitting server thread");
+                            eprintln!("Quitting server thread {}", listener.local_addr().unwrap());
                             break;
                         }
                         Ok(MessageType::SendChat(text)) => {
-                            for (_, connection_sender) in connection_threads.iter() {
-                                connection_sender.send(MessageType::SendChat(text.clone())).unwrap();
-                            }
+                            connection_threads.retain(|(_, connection_sender)| {
+                                if let Err(_) = connection_sender.send(MessageType::SendChat(text.clone())) {
+                                    false
+                                } else {
+                                    true
+                                }
+                            });
                         }
                         Err(_) => {
-                            // TODO: Replace with something smarter
+                            // TODO: Replace with something smarter and OS dependant
                             sleep(Duration::from_millis(10));
                         }
                     }
@@ -160,41 +207,49 @@ fn main() -> std::io::Result<()> {
                 Err(error) => eprintln!("Error when accepting listeners: {}", error),
             }
         }
+
+        Ok(())
     });
 
-    let (client_sender, client_receiver): (Sender<MessageType>, Receiver<MessageType>) = channel();
-    let connect_addr_str = &args.get(2);
-    match connect_addr_str {
-        Some(connect_addr_str) => {
-            println!("Connecting to {}", connect_addr_str);
-        }
-        None => {
-            println!("Quitting client");
-        }
+    let quit_requested = Arc::new(AtomicBool::new(false));
+    {
+        let quit_requested = quit_requested.clone();
+        ctrlc::set_handler(move || {
+            if quit_requested.load(Ordering::SeqCst) {
+                eprintln!("\rExiting forcefully");
+                exit(1);
+            } else {
+                eprintln!("\rShutdown requested");
+                quit_requested.store(true, Ordering::SeqCst);
+            }
+        }).unwrap();
     }
-    let client_thread = spawn(move || {});
 
     loop {
-        print!("> ");
-        stdout().flush().unwrap();
-        let mut input = String::new();
-        stdin().read_line(&mut input).unwrap();
-        let input = input.trim();
-        if input == "q" {
+        if quit_requested.load(Ordering::SeqCst) {
             server_sender.send(MessageType::SendChat("Bye".to_string())).unwrap();
-            println!("Quitting...");
             server_sender.send(MessageType::Quit).unwrap();
+            server_thread.join().unwrap().unwrap();
             break;
-        } else {
-            println!("[you] {}", input);
-            server_sender.send(MessageType::SendChat(input.to_string())).unwrap();
+        }
+
+        match crossterm::event::poll(Duration::from_secs(0)).unwrap() {
+            true => {
+                // It's guaranteed that read() wont block if `poll` returns `Ok(true)`
+                let command = read_line().unwrap();
+
+                if command == "ping" || command == "p" {
+                    server_sender.send(MessageType::SendChat("ping".to_string())).unwrap();
+                } else if command == "quit" || command == "q" {
+                    quit_requested.store(true, Ordering::SeqCst);
+                }
+            }
+            false => {
+                // TODO: Replace with something smarter and OS dependant
+                sleep(Duration::from_millis(10));
+            }
         }
     }
-
-    println!("Waiting for client");
-    client_thread.join().unwrap();
-    println!("Waiting for server");
-    server_thread.join().unwrap();
 
     Ok(())
 }
